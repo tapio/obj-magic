8b57f9c70e965332816fac88b705f261e5ee6b7d
Tapio Vierros <tapiovierros@gmail.com>
2012-01-10 23:56:24 +0200

    Can now center vertices along given axes. Uses now proper commandline parser.

---
+++ args.hpp
@@ -0,0 +1,87 @@
+#pragma once
+
+#include <string>
+#include <sstream>
+#include <set>
+#include <vector>
+
+class Args {
+public:
+	Args(int argc, char* argv[]): app_name(argv[0])
+	{
+		for (int i = 1; i < argc; ++i) {
+			std::string arg(argv[i]);
+			int l = arg.length();
+			if (l == 0) continue;
+			if (arg[0] == '-' && l >= 2) {
+				if (arg[1] == '-') { // Long opt
+					arg = arg.substr(2);
+					size_t pos = arg.find('=');
+					if (pos != std::string::npos) {
+						longopts.insert(arg.substr(0, pos));
+						allopts.push_back(arg.substr(0, pos));
+						allopts.push_back(arg.substr(pos+1));
+						continue;
+					}
+					longopts.insert(arg);
+					allopts.push_back(arg);
+				} else { // Short opt
+					for (int j = 1; j < l; ++j) {
+						if (arg[j] == '=') { // Handle argument format: -a=123
+							allopts.push_back(arg.substr(j+1));
+							break;
+						}
+						shortopts.insert(arg[j]);
+						allopts.push_back(std::string(arg[j], 1));
+					}
+				}
+			} else if (arg[0] != '-') {
+				globalopts.insert(arg);
+				allopts.push_back(arg);
+			}
+		}
+	}
+
+	bool opt(char shortopt, std::string longopt) {
+		if (shortopt && shortopt != ' ' && shortopts.count(shortopt)) return true;
+		if (!longopt.empty() && longopts.count(longopt)) return true;
+		return false;
+	}
+
+	template<typename T> // std::string specialized below this class
+	T arg(char shortopt, std::string longopt, T default_arg = T()) {
+		for (std::vector<std::string>::const_iterator it = allopts.begin(); it != allopts.end(); ++it) {
+			if (*it == std::string(shortopt, 1) || *it == longopt) {
+				++it;
+				if (it == allopts.end() || (*it)[0] == '-') return default_arg;
+				T ret = T();
+				std::istringstream iss(*it);
+				iss >> ret;
+				return ret;
+			}
+		}
+		return default_arg;
+	}
+
+	std::string app() const { return app_name; }
+
+private:
+	std::string app_name;
+	std::vector<std::string> allopts;
+	std::set<char> shortopts;
+	std::set<std::string> longopts;
+	std::set<std::string> globalopts;
+};
+
+
+template<>
+std::string Args::arg<std::string>(char shortopt, std::string longopt, std::string default_arg) {
+	for (std::vector<std::string>::const_iterator it = allopts.begin(); it != allopts.end(); ++it) {
+		if (*it == std::string(shortopt, 1) || *it == longopt) {
+			++it;
+			if (it == allopts.end() || (*it)[0] == '-') return default_arg;
+			return *it;
+		}
+	}
+	return default_arg;
+}
--- wavefront-modify.cpp
+++ wavefront-modify.cpp
@@ -3,31 +3,81 @@
 #include <fstream>
 #include <sstream>
 #include <cstdlib>
+#include <limits>
+
+#include "args.hpp"
+
+enum Commands {
+	NONE = 0,
+	SCALE = 1,
+	CENTERX = 2,
+	CENTERY = 4,
+	CENTERZ = 8
+};
 
 int main(int argc, char* argv[]) {
-	if (argc != 3) {
-		std::cerr << "Usage:    " << argv[0] << " FILE SCALE" << std::endl;
-		std::cerr << "Example:  " << argv[0] << " model.obj 0.5" << std::endl;
+	Args args(argc, argv);
+	if (args.opt('h', "help") || argc < 3) {
+		std::cerr << "Usage:    " << args.app() << " COMMAND [PARAMS] FILE" << std::endl;
+		std::cerr << "Example:  " << args.app() << " --scale 0.5 model.obj" << std::endl;
 		return EXIT_FAILURE;
 	}
-	float scale = atof(argv[2]);
-	if (scale <= 0.0f) {
-		std::cerr << "Invalid scale" << std::endl;
-		return EXIT_FAILURE;
-	}
-	std::string row;
-	std::ifstream file(argv[1], std::ios::binary);
+	int mode = NONE;
+
+	float scale = args.arg('s', "scale", -1.0f);
+	if (scale > 0.0f) mode |= SCALE;
+	if (args.opt('c', "center")) mode |= CENTERX | CENTERY | CENTERZ;
+	if (args.opt(' ', "centerx")) mode |= CENTERX;
+	if (args.opt(' ', "centery")) mode |= CENTERY;
+	if (args.opt(' ', "centerz")) mode |= CENTERZ;
+
+	std::string filename = argv[argc-1];
+	std::ifstream file(filename.c_str(), std::ios::binary);
 	if (!file.is_open()) {
-		std::cerr << "Failed to open file " << argv[1] << std::endl;
+		std::cerr << "Failed to open file " << filename << std::endl;
 		return EXIT_FAILURE;
 	}
+
+	std::string row;
+	// Analyzing pass
+	float xmin = std::numeric_limits<float>::max();
+	float xmax = -std::numeric_limits<float>::max();
+	float ymin = std::numeric_limits<float>::max();
+	float ymax = -std::numeric_limits<float>::max();
+	float zmin = std::numeric_limits<float>::max();
+	float zmax = -std::numeric_limits<float>::max();
 	while (getline(file, row)) {
 		std::istringstream srow(row);
 		float x,y,z;
 		std::string tempst;
 		if (row.substr(0,2) == "v ") {  // Vertices
 			srow >> tempst >> x >> y >> z;
-			std::cout << "v " << x*scale << " " << y*scale << " " << z*scale << std::endl;
+			xmin = std::min(x, xmin);
+			ymin = std::min(y, ymin);
+			zmin = std::min(z, zmin);
+			xmax = std::max(x, xmax);
+			ymax = std::max(y, ymax);
+			zmax = std::max(z, zmax);
+		}
+	}
+	float cx = (xmin + xmax) * 0.5;
+	float cy = (ymin + ymax) * 0.5;
+	float cz = (zmin + zmax) * 0.5;
+
+	// Output pass
+	file.clear();
+	file.seekg(0, std::ios::beg);
+	while (getline(file, row)) {
+		std::istringstream srow(row);
+		float x,y,z;
+		std::string tempst;
+		if (row.substr(0,2) == "v ") {  // Vertices
+			srow >> tempst >> x >> y >> z;
+			if (mode & CENTERX) x -= cx;
+			if (mode & CENTERY) y -= cy;
+			if (mode & CENTERZ) z -= cz;
+			if (mode & SCALE) { x *= scale; y *= scale; z *= scale; }
+			std::cout << "v " << x << " " << y << " " << z << std::endl;
 		} else {
 			std::cout << row << std::endl;
 		}
