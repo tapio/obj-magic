3293d469ce301a4b7b36d4ddec3f744ac9caf945
Tapio Vierros <tapiovierros@gmail.com>
2012-01-11 19:08:28 +0200

    Fallback to g++ if clang++ is not available.
    Also, if CXX env variable is set, that is used instead.

--- make.sh
+++ make.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 
 EXENAME="obj-magic"
-CXX=clang++
 CFLAGS="-O2 -Wall -Wextra -Wno-unused-parameter"
+
+if [ "x$CXX" = "x" ]; then
+	# Default to GCC
+	CXX=g++
+	# Prefer clang++ if available
+	which clang++ > /dev/null
+	if [ $? -eq 0 ]; then
+		CXX=clang++
+	fi
+fi
+
 $CXX $CFLAGS *.cpp -o $EXENAME
+
