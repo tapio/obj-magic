01029b7664c9ec60a6f657f6ae393301809ce149
Tapio Vierros <tapiovierros@gmail.com>
2016-01-07 19:45:17 +0200

    Include only needed glm headers.

--- src/obj-magic.cpp
+++ src/obj-magic.cpp
@@ -7,7 +7,10 @@
 #include <limits>
 #include <map>
 
-#include "../glm/glm.hpp"
+#include "../glm/vec2.hpp"
+#include "../glm/vec3.hpp"
+#include "../glm/mat3x3.hpp"
+#include "../glm/mat4x4.hpp"
 #include "../glm/gtc/matrix_transform.hpp"
 #include "../glm/gtx/component_wise.hpp"
 #include "args.hpp"
